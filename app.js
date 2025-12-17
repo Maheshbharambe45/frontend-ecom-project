bb81d411ebe27fb612b945bbb763c1a8b35bd8b2
Mahesh_Bharambe <144551859+Maheshbharambe45@users.noreply.github.com>
2025-12-17 12:25:12 +0530

    Add console log for pipeline update message

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("hello from app.js");
 console.log("Hello form jenkins repository")
 console.log("heyy buddy from github")
 console.log("heyy buddy from pipeline")
+console.log("heyy Mahesh these message coming after the updating the pipelines")
